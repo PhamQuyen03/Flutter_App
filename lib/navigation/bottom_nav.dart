99afe884077b4b8bc28d84c0ad88779a8d3a0d73
dangduoc <dangduoc@outlook.com>
2020-11-15 21:34:50 +0700

    refactor root navigation

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,77 +1,31 @@
 import 'package:flutter/material.dart';
 
-enum TabItem { home, test, search, trivia, settings }
-
-Map<TabItem, String> tabName = {
-  TabItem.home: 'Home',
-  TabItem.test: 'Test',
-  TabItem.search: 'Search',
-  //TabItem.speech: 'Speech',
-  TabItem.trivia: 'Trivia',
-  TabItem.settings: 'Settings'
-};
+import 'navigation.dart';
 
 class BottomNavigation extends StatelessWidget {
   BottomNavigation({this.currentTab, this.onSelectTab});
-  final TabItem currentTab;
-  final ValueChanged<TabItem> onSelectTab;
+  final RouteItem currentTab;
+  final ValueChanged<RouteItem> onSelectTab;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       backgroundColor: Colors.white,
       elevation: 1,
       type: BottomNavigationBarType.fixed,
-      items: [
-        _buildItem(tabItem: TabItem.home),
-        _buildItem(tabItem: TabItem.test),
-        _buildItem(tabItem: TabItem.search),
-        _buildItem(tabItem: TabItem.trivia),
-        _buildItem(tabItem: TabItem.settings),
-      ],
-      onTap: (index) => onSelectTab(
-        TabItem.values[index],
-      ),
-      currentIndex: currentTab.index,
+      items: _buildBottomNavigationItems(),
+      onTap: (index) => onSelectTab(NavigationSetting.appTabList[index]
+          // RouteItem.values[index],
+          ),
+      currentIndex: NavigationSetting.appTabList.indexOf(currentTab),
     );
   }
 
-  BottomNavigationBarItem _buildItem({TabItem tabItem}) {
-    String text = tabName[tabItem];
-    IconData icon;
-    switch (tabItem) {
-      case TabItem.home:
-        {
-          icon = Icons.home;
-          break;
-        }
-      case TabItem.test:
-        {
-          icon = Icons.list;
-          break;
-        }
-      case TabItem.search:
-        {
-          icon = Icons.search;
-          break;
-        }
-      case TabItem.trivia:
-        {
-          icon = Icons.layers;
-          break;
-        }
-      case TabItem.settings:
-        {
-          icon = Icons.settings;
-          break;
-        }
-    }
-    return BottomNavigationBarItem(
-      icon: Icon(
-        icon,
-      ),
-      title: Text(
-        text,
-      ),
-    );
+  _buildBottomNavigationItems() {
+    List<BottomNavigationBarItem> items = new List<BottomNavigationBarItem>();
+    NavigationSetting.appTabs.forEach((key, value) {
+      items.add(
+          BottomNavigationBarItem(icon: Icon(value.icon), label: value.title));
+    });
+    return items;
   }
 }
--- lib/navigation/route_generator.dart
+++ lib/navigation/route_generator.dart
@@ -1,144 +1,138 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/features/authentication/presentation/pages/login_page.dart';
-import 'package:flutter_app/features/authentication/presentation/pages/phone_number_verify_code.dart';
-import 'package:flutter_app/features/authentication/presentation/pages/register_page.dart';
-import 'package:flutter_app/features/authentication/presentation/pages/register_succeed_page.dart';
-import 'package:flutter_app/features/authentication/presentation/widgets/verify_code.dart';
-import 'package:flutter_app/features/home/presentation/pages/top_students.dart';
-import 'package:flutter_app/features/search/presentation/pages/search_page.dart';
 import 'package:flutter_app/features/speech_recognition/presentation/pages/speech_recognition_page.dart';
-import 'package:flutter_app/features/test/presentation/pages/test_page.dart';
-
+import '../features/authentication/presentation/pages/login_page.dart';
+import '../features/authentication/presentation/pages/phone_number_verify_code.dart';
+import '../features/authentication/presentation/pages/register_page.dart';
+import '../features/authentication/presentation/pages/register_succeed_page.dart';
 import '../features/home/presentation/pages/home_page.dart';
 import '../features/home/presentation/pages/post_detail.dart';
 import '../features/number_trivia/presentation/pages/number_trivia_page.dart';
+import '../features/search/presentation/pages/search_page.dart';
 import '../features/settings/presentation/pages/settings_page.dart';
+import '../features/test/presentation/pages/test_page.dart';
+import 'navigation.dart';
 
-class HomeRouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-    switch (settings.name) {
-      case '/':
-        {
-          return MaterialPageRoute(builder: (_) => HomePage());
-        }
-      case '/post/detail':
-        {
-          return MaterialPageRoute(builder: (_) => PostDetail());
-        }
-      default:
-        {
-          return _errorRoute();
-        }
-    }
-  }
-}
-
-// class SpeechRouteGenerator {
-//   static Route<dynamic> generateRoute(RouteSettings settings) {
-//     final args = settings.arguments;
-//     switch (settings.name) {
-//       case '/':
-//         {
-//           return MaterialPageRoute(builder: (_) => SpeechRecognitionPage());
-//         }
-//       default:
-//         {
-//           return _errorRoute();
-//         }
-//     }
-//   }
+// Map<TabItem,Route<dynamic>> appRoutes ={
+//   TabItem.home:
 // }
-class TestRouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-    switch (settings.name) {
-      case '/':
-        {
-          return MaterialPageRoute(builder: (_) => TestPage());
-        }
-      default:
-        {
-          return _errorRoute();
-        }
-    }
+
+Route<dynamic> getTabRoute(RouteItem tabItem, RouteSettings settings) {
+  switch (tabItem) {
+    case RouteItem.home:
+      return _homeRoute(settings);
+    case RouteItem.test:
+      return _testRoute(settings);
+    case RouteItem.search:
+      return _searchRoute(settings);
+    case RouteItem.trivia:
+      return _triviaRoute(settings);
+    case RouteItem.settings:
+      return _settingRoute(settings);
+    case RouteItem.speech:
+      return _speechToTextRoute(settings);
+    default:
+      return _errorRoute();
   }
 }
 
-class SearchRouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-    switch (settings.name) {
-      case '/':
-        {
-          return MaterialPageRoute(builder: (_) => SearchPage());
-        }
-      default:
-        {
-          return _errorRoute();
-        }
-    }
+Route<dynamic> _homeRoute(RouteSettings settings) {
+  final args = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => HomePage());
+      }
+    case '/post/detail':
+      {
+        return MaterialPageRoute(builder: (_) => PostDetail());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
   }
 }
 
-class TriviaRouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-    switch (settings.name) {
-      case '/':
-        {
-          return MaterialPageRoute(builder: (_) => NumberTriviaPage());
-        }
-      default:
-        {
-          return _errorRoute();
-        }
-    }
+Route<dynamic> _testRoute(RouteSettings settings) {
+  final args = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => TestPage());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
   }
 }
 
-class SettingsRouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    final args = settings.arguments;
-    switch (settings.name) {
-      case '/':
-        {
-          return MaterialPageRoute(builder: (_) => SettingsPage());
-        }
-      default:
-        {
-          return _errorRoute();
-        }
-    }
+Route<dynamic> _searchRoute(RouteSettings settings) {
+  final args = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => SearchPage());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
   }
 }
 
-class AuthenticationRouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/':
-        {
-          return MaterialPageRoute(builder: (_) => LoginPage());
-        }
-      case '/register':
-        {
-          return MaterialPageRoute(builder: (_) => RegisterPage());
-        }
-      case '/register/verifycode':
-        {
-          return MaterialPageRoute(
-              builder: (_) => PhoneNumberVerifyPage(
-                  settings.arguments as FormVerifyCodeArguments));
-        }
-      case '/register/succeed':
-        {
-          return MaterialPageRoute(builder: (_) => RegisterSucceedPage());
-        }
-      default:
-        {
-          return _errorRoute();
-        }
-    }
+Route<dynamic> _triviaRoute(RouteSettings settings) {
+  final args = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => NumberTriviaPage());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
+  }
+}
+
+Route<dynamic> _settingRoute(RouteSettings settings) {
+  final args = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => SettingsPage());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
+  }
+}
+
+Route<dynamic> authenticationRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => LoginPage());
+      }
+    case '/register':
+      {
+        return MaterialPageRoute(builder: (_) => RegisterPage());
+      }
+    case '/register/verifycode':
+      {
+        return MaterialPageRoute(
+            builder: (_) => PhoneNumberVerifyPage(
+                settings.arguments as FormVerifyCodeArguments));
+      }
+    case '/register/succeed':
+      {
+        return MaterialPageRoute(builder: (_) => RegisterSucceedPage());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
   }
 }
 
@@ -153,4 +147,18 @@ Route<dynamic> _errorRoute() {
       ),
     );
   });
+}
+
+Route<dynamic> _speechToTextRoute(RouteSettings settings) {
+  final args = settings.arguments;
+  switch (settings.name) {
+    case '/':
+      {
+        return MaterialPageRoute(builder: (_) => SpeechRecognitionPage());
+      }
+    default:
+      {
+        return _errorRoute();
+      }
+  }
 }
